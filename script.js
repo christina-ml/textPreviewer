d64f978445d87b88f097aa51ae82ec220e960f88
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-13 12:06:28 -0400

    Selected fonts, selected colors. Added event listener on input to textarea box. Added event listeners on change for dropdown options - for both fonts and colors

--- script.js
+++ script.js
@@ -171,3 +171,82 @@ const colors = [
 // https://www.colourlovers.com/api/colors/random&format=json
 
 // Add Javascript Here
+
+/* SELECT FONTS */
+let fontSelection = document.querySelector("#font-selection");
+let previewPlaceholder = document.querySelector("#preview-placeholder");
+// Array of fonts. For each of these, we will create an element.
+for (let font of fonts) {
+    let option = document.createElement("option");
+    option.textContent = font;
+    option.value = font;
+    // option.style.fontFamily = font // josh: try to change font on selection
+    fontSelection.append(option);
+}
+
+/* Change the `previewPlaceholder` to selected option */
+fontSelection.addEventListener("change", (e)=>{
+    // console.log("trigger", e.target.value);
+    previewPlaceholder.style.fontFamily = e.target.value;
+    // console.log(previewPlaceholder.style.fontFamily)
+})
+
+
+/* SELECT COLORS */
+let colorSelection = document.querySelector("#color-selection");
+for (let color of colors) {
+    let option = document.createElement("option");
+    option.textContent = color;
+    option.value = color;
+    // option.style.color = color; // josh: try to change font on selection
+    colorSelection.append(option);
+}
+
+/* Change the `previewPlaceholder` to selected option */
+colorSelection.addEventListener("change", (e)=>{
+    // console.log("trigger", e.target.value);
+    previewPlaceholder.style.color = e.target.value;
+    // console.log(previewPlaceholder.style.color)
+})
+
+/* Add an Event Listener to the `textarea` box */
+let textInput = document.querySelector("#text-input");
+textInput.addEventListener("input", (e)=>{
+    /* e.target.value is extremely common (will be using it heavily in the future) */
+    // console.log("Trigger", e.target.value);
+    previewPlaceholder.textContent = e.target.value;
+})
+
+
+
+
+let saveButton = document.querySelector("#save-button");
+let savedList = document.querySelector("#saved-list");
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Kiyomi:
+loading font & color
+
+
+while typing in `#text-input` value needs to be in preview
+Add text to preview
+Add font to preview - selecting font - array of fonts
+Add color to preview - selecting color - array of colors
+`#save-button` - add event listener
+add text from preview to `#saved-list`
+*/
+
+/* christina found this:
+https://www.thecolorapi.com/id?hex=24B1E0
+*/
